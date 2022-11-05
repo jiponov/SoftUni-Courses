c5e40a7ace647718a3c211fa7bde4b6b594deb09
jiponov <lubomir.jiponov@gmail.com>
2022-11-05 02:27:56 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/01-Basic Syntax, Conditional Statements and Loops/10-lab-10.numbersFromMToN.js
@@ -0,0 +1,16 @@
+function numbersFromMToN(m, n) {
+
+    for (let i = m; i >= n; i--) {
+        console.log(i)
+    }
+
+    /*
+    let i = m;
+    while (i >= n) {
+        console.log(i)
+        i--;
+    }
+    */
+}
+numbersFromMToN(6, 2);
+numbersFromMToN(4, 1);
