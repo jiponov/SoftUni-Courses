60a3c98adc9ba99222e745db18239a792b30306a
jiponov <lubomir.jiponov@gmail.com>
2022-11-18 15:51:23 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/01-Basic Syntax, Conditional Statements and Loops/30-exeM-04.(c).reverseString.js
@@ -0,0 +1,8 @@
+function reverseString(input) {
+    let output = input.toString().split("").reverse().join("");
+    console.log(output);
+}
+reverseString('Hello');
+reverseString('SoftUni');
+reverseString('1234');
+
