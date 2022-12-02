509507ec8a49e6a5e431cca555b919c4fb0ef2dd
jiponov <lubomir.jiponov@gmail.com>
2022-12-02 11:31:06 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/02-Data Types and Variables/18-exe-02.charstoString.js
@@ -0,0 +1,8 @@
+function charsToString(a, b, c) {
+    let combine = a + b + c;
+    console.log(combine)
+}
+charsToString('a', 'b', 'c');
+charsToString('%', '2', 'o');
+charsToString('1', '5', 'p');
+
