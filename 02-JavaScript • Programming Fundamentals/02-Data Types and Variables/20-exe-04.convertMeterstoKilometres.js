715ef5e39044ee62e1d9e996c5d61e4d2b33ef9d
jiponov <lubomir.jiponov@gmail.com>
2022-12-05 12:03:02 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/02-Data Types and Variables/20-exe-04.convertMeterstoKilometres.js
@@ -0,0 +1,6 @@
+function convertMeterstoKilometres(num) {
+    let mToKm = (num / 1000).toFixed(2);
+    console.log(`${mToKm}`);
+}
+convertMeterstoKilometres(1852);
+convertMeterstoKilometres(798);
