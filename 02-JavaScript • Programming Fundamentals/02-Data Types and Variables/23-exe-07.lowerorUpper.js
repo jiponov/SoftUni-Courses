01e45031daa31f9797fefec015830e0db92f9a50
jiponov <lubomir.jiponov@gmail.com>
2022-12-08 10:43:05 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/02-Data Types and Variables/23-exe-07.lowerorUpper.js
@@ -0,0 +1,10 @@
+function lowerorUpper(char) {
+
+    if (char == char.toUpperCase()) {
+        console.log('upper-case')
+    } else {
+        console.log('lower-case')
+    }
+}
+lowerorUpper('L');
+lowerorUpper('f');
