79e8931cf7cb4a4189596476522c24a9d61be627
jiponov <lubomir.jiponov@gmail.com>
2022-12-20 10:57:08 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/03-Arrays/08-lab-08.condenseArrayToNumber.js
@@ -0,0 +1,21 @@
+function condenseArrayToNumber(inputArr) {
+
+    let condensed = [];
+    for (let el of inputArr) {
+        condensed.push(el)
+    }
+
+    while (condensed.length > 1) {
+        let temp = [];
+        for (let i = 0; i < condensed.length - 1; i++) {
+            temp.push(condensed[i] + condensed[i + 1])
+        }
+        condensed = temp;
+    }
+    console.log(condensed[0]);
+
+}
+
+condenseArrayToNumber([2, 10, 3]);
+condenseArrayToNumber([5, 0, 4, 1, 2]);
+condenseArrayToNumber([1]);
