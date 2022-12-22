cac0cc2814a78abc299bf290cc8eeb255b7c1ff7
jiponov <lubomir.jiponov@gmail.com>
2022-12-23 00:01:12 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/03-Arrays/11-exe-03.mergeArrays.js
@@ -0,0 +1,16 @@
+function mergeArrays(arr1, arr2) {
+
+    let output = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (i % 2 === 0) {
+            output.push(Number(arr1[i]) + Number(arr2[i]));
+        } else {
+            output.push(`${arr1[i]}${arr2[i]}`);
+        }
+    }
+    console.log(output.join(' - '));
+}
+
+mergeArrays(['5', '15', '23', '56', '35'], ['17', '22', '87', '36', '11']);
+mergeArrays(['13', '12312', '5', '77', '4'], ['22', '333', '5', '122', '44']);
