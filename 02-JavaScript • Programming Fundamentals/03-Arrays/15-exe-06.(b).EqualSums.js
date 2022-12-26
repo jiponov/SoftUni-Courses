7990b20ceda35da379ac59a21a34930b1165d0ed
jiponov <lubomir.jiponov@gmail.com>
2022-12-26 23:54:01 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/03-Arrays/15-exe-06.(b).EqualSums.js
@@ -0,0 +1,36 @@
+function EqualSums(arr) {
+    let output;
+    let areEqual = false;
+
+    for (let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+
+        for (let j = i - 1; j >= 0; j--) {
+            leftSum += arr[j];
+        }
+
+        for (let k = i + 1; k < arr.length; k++) {
+            rightSum += arr[k];
+        }
+
+        if (leftSum === rightSum) {
+            output = i;
+            areEqual = true;
+        }
+    }
+
+    if (areEqual) {
+        console.log(output)
+    } else {
+        console.log('no');
+    }
+}
+
+EqualSums([1, 2, 3, 3]);
+EqualSums([1, 2]);
+EqualSums([1]);
+EqualSums([1, 2, 3]);
+EqualSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
+
+
