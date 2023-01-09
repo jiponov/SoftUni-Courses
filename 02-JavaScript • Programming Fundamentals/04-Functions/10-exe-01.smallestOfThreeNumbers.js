7b09876ff1359cf994599a9b579463bda3e4f14f
jiponov <lubomir.jiponov@gmail.com>
2023-01-09 11:24:10 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/04-Functions/10-exe-01.smallestOfThreeNumbers.js
@@ -0,0 +1,10 @@
+function smallestOfThreeNumbers(a, b, c) {
+
+    let output = Math.min(a, b, c);             // Math.min и Math.max  с повече от два параметъра
+    console.log(output);
+}
+
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
