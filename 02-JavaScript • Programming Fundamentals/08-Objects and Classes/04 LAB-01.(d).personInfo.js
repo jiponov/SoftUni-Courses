255ce3c490c256352aa4d8de7527a31fd96787f0
jiponov <lubomir.jiponov@gmail.com>
2023-02-16 23:18:09 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/08-Objects and Classes/04 LAB-01.(d).personInfo.js
@@ -0,0 +1,13 @@
+function personInfo(firstName, lastName, age) {
+    let person = {};
+
+    person.firstName = firstName;
+    person['lastName'] = lastName;
+    person.age = age;
+
+    return person;
+}
+console.log(personInfo("Peter", "Pan", "20"));
+console.log('---');
+console.log(personInfo("George", "Smith", "18"));
+
