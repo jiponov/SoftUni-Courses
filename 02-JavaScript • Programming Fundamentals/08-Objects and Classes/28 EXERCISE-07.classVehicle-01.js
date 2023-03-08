f81a44bbb400611633ae01c2abad20540c8e8624
jiponov <lubomir.jiponov@gmail.com>
2023-03-08 14:21:27 +0200

    fund

---
+++ 02-JavaScript • Programming Fundamentals/08-Objects and Classes/28 EXERCISE-07.classVehicle-01.js
@@ -0,0 +1,27 @@
+function classVehicle() {
+    // copy за judge от тук
+    class Vehicle {
+        constructor(type, model, parts, fuel) {
+            this.type = type;
+            this.model = model;
+            this.parts = parts;
+            this.fuel = fuel;
+            this.parts.quality = this.parts.engine * this.parts.power;
+        }
+
+        drive(lostFuel) {
+            return this.fuel -= lostFuel;
+        }
+    }
+
+  
+    let parts = { engine: 6, power: 100 };
+    let vehicle = new Vehicle('van', 's8', parts, 200);
+    vehicle.drive(100);
+    vehicle.drive(50);
+
+    console.log(vehicle.fuel);
+    console.log(vehicle.parts.quality);
+
+}
+classVehicle();
