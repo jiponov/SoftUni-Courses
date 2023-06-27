129d2a73513d3f021209808e1a9fee5c9b376e34
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2023-06-27 14:34:10 +0300

    fund

---
+++ 02-JavaScript • Programming Fundamentals/10-Text Processing/2.exe-06.pascal-CaseSplitter-03.js
@@ -0,0 +1,16 @@
+function pascalCaseSplitter(text) {
+    let result = text[0];
+
+    let lower = text.toLocaleLowerCase();
+
+    for (let i = 1; i < text.length; i++) {
+        if (text[i] != lower[i]) {
+            result += ', ';
+        }
+        result += text[i];
+    }
+
+    console.log(result);
+}
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
+pascalCaseSplitter('HoldTheDoor');
