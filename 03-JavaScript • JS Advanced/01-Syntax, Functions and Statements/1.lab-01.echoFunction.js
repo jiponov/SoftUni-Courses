b82e5e2f98e1de16a5c9f219204aec16706de2d3
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2023-07-29 23:59:42 +0300

    js advanced

---
+++ 03-JavaScript • JS Advanced/01-Syntax, Functions and Statements/1.lab-01.echoFunction.js
@@ -0,0 +1,19 @@
+function echoFunction(str) {    // 100/100
+
+    let strLength = str.length;
+
+    console.log(strLength);
+    console.log(str);
+}
+
+echoFunction('Hello, JavaScript!');
+console.log('-------');
+echoFunction('strings are easy');
+
+/*
+
+•	Write a function that receives a single parameter.
+•	Use the console.log function to print text on the console. Each call prints a newline automatically.
+•	The string’s length property is used to determine how many characters are in a given string
+
+*/
