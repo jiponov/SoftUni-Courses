9be927c99a664a424125faca384cde0b29b87680
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2023-09-14 16:14:08 +0300

    js advanced

---
+++ 03 JavaScript • JS Advanced/04-DOM intro/04 LAB-04.showMore/showMore-1.js
@@ -0,0 +1,7 @@
+function showMore() {    // 100/100   
+    const readmoreElement = document.getElementById('more');
+    const textElement = document.getElementById('text');
+    readmoreElement.style.display = 'none';
+    textElement.style.display = 'inline';
+}
+
