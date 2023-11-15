8413dc3eea39822b1c307c0818c40ceb752e454e
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2023-11-16 00:48:50 +0200

    js advanced

---
+++ 03 JavaScript • JS Advanced/07-Unit Testing and Error Handling/06 LAB-06.rgbToHex/rgbToHex.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const { rgbToHexColor } = require('./rgbToHex.js');
+
+
+describe('rgbToHex', () => {                // 100/100
+
+    it('converts to black, down bound', () => {
+        expect(rgbToHexColor(0, 0, 0)).to.be.equal('#000000');
+    });
+
+    it('converts to white, upper bound', () => {
+        expect(rgbToHexColor(255, 255, 255)).to.be.equal('#FFFFFF');
+    });
+
+    it('return undefined for out of upper bound', () => {
+        expect(rgbToHexColor(256, 256, 256)).to.be.undefined;
+    });
+
+    it('return undefined for out of down bound', () => {
+        expect(rgbToHexColor(-1, -1, -1)).to.be.undefined;
+    });
+
+    it('return undefined for invalid type params', () => {
+        expect(rgbToHexColor('0', '0', '0')).to.be.undefined;
+    });
+
+    it('return undefined for floats', () => {
+        expect(rgbToHexColor(1.1, 1.1, 1.1)).to.be.undefined;
+    });
+
+    it('return undefined for missing params', () => {
+        expect(rgbToHexColor(0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0)).to.be.undefined;
+        expect(rgbToHexColor()).to.be.undefined;
+    });
+});
