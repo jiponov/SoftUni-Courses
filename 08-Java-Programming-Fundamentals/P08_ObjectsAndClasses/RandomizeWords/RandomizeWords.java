fdbf62bc1f78be49cee62c73fa239dc97675a413
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-02-26 22:15:00 +0200

    java fund

---
+++ 08-Java-Programming-Fundamentals/P08_ObjectsAndClasses/RandomizeWords/RandomizeWords.java
@@ -0,0 +1,25 @@
+package P08_ObjectsAndClasses.RandomizeWords;
+// 100/100
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomizeWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] sentence = scanner.nextLine().split(" ");
+
+        Random random = new Random();
+        for (int i = 0; i < sentence.length; i++) {
+            int randomIndex = random.nextInt(sentence.length);
+            String temp = sentence[i];
+            sentence[i] = sentence[randomIndex];
+            sentence[randomIndex] = temp;
+        }
+
+        for (String word : sentence) {
+            System.out.println(word);
+        }
+    }
+}
