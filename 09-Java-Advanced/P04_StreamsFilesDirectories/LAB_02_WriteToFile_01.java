ddca797707dc4ba1c101e2ddf46967c53a4301d3
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-06-15 23:45:22 +0300

    java advanced

---
+++ 09-Java-Advanced/P04_StreamsFilesDirectories/LAB_02_WriteToFile_01.java
@@ -0,0 +1,32 @@
+package P04_StreamsFilesDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class LAB_02_WriteToFile_01 {
+    public static void main(String[] args) throws IOException {
+
+        String pathRead = "C:\\Users\\I353529\\Documents\\SoftUni\\Projects\\Java-Adv-May-2023\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String pathWrite = "C:\\Users\\I353529\\Documents\\SoftUni\\Projects\\Java-Adv-May-2023\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\outputTask2.txt";
+
+        FileInputStream inputStream = new FileInputStream(pathRead);
+        FileOutputStream outputStream = new FileOutputStream(pathWrite);
+
+        int currentByte = inputStream.read();   // ascii кода на прочетения символ
+        while (currentByte >= 0) {
+            char currentSymbol = (char) currentByte;
+
+            if (currentSymbol != '.' && currentSymbol != ',' && currentSymbol != '!' && currentSymbol != '?') {
+                outputStream.write(currentSymbol);
+            }
+
+            currentByte = inputStream.read();
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+}
