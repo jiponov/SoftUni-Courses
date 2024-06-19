af840a5abdd6dc1ac375f3164d7c148ed6f9eee5
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-06-19 23:54:38 +0300

    java advanced

---
+++ 09-Java-Advanced/P04_StreamsFilesDirectories/Cube.java
@@ -0,0 +1,18 @@
+package P04_StreamsFilesDirectories;
+
+import java.io.Serializable;
+
+public class Cube implements Serializable {
+
+    private String color;
+    private int width;
+    private int length;
+    private int height;
+
+    public Cube(String color, int width, int length, int height) {
+        this.color = color;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+}
