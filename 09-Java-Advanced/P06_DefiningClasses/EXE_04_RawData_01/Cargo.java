592794a704983d199e250a2228b12bc410a190de
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-08-04 09:53:34 +0300

    java

---
+++ 09-Java-Advanced/P06_DefiningClasses/EXE_04_RawData_01/Cargo.java
@@ -0,0 +1,11 @@
+package P06_DefiningClasses.EXE_04_RawData_01;
+
+public class Cargo {
+    private int cargoWeight;
+    private String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+}
---
+++ 09-Java-Advanced/P06_DefiningClasses/EXE_04_RawData_01/Engine.java
@@ -0,0 +1,15 @@
+package P06_DefiningClasses.EXE_04_RawData_01;
+
+public class Engine {
+    private int engineSpeed;
+    private int enginePower;
+
+    public Engine(int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+}
