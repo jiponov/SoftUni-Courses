93812ce582544cadfaf943d2f03286b2c6c28bb6
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-08-23 10:25:31 +0300

    java

---
+++ 09-Java-Advanced/P07_Generics/EXE_05_GenericCountMethodString/Main.java
@@ -0,0 +1,26 @@
+package P07_Generics.EXE_05_GenericCountMethodString;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Box<String> box = new Box<String>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            //int number = Integer.parseInt(scanner.nextLine());
+            String text = scanner.nextLine();
+            box.add(text);
+        }
+
+//        String indexes = scanner.nextLine();
+//        int swapIndex1 = Integer.parseInt(indexes.split("\\s+")[0]);
+//        int swapIndex2 = Integer.parseInt(indexes.split("\\s+")[1]);
+//        box.swap(swapIndex1, swapIndex2);
+
+        String element = scanner.nextLine();
+        System.out.println(box.countGreaterThan(element));
+    }
+}
