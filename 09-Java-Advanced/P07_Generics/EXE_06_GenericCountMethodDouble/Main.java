39490dccde62f727b8aeb67bf443888cf5f0a18e
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-08-25 11:34:58 +0300

    java

---
+++ 09-Java-Advanced/P07_Generics/EXE_06_GenericCountMethodDouble/Main.java
@@ -0,0 +1,27 @@
+package P07_Generics.EXE_06_GenericCountMethodDouble;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Box<Double> box = new Box<Double>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            // int number = Integer.parseInt(scanner.nextLine());
+            double number = Double.parseDouble(scanner.nextLine());
+            // String text = scanner.nextLine();
+            box.add(number);
+        }
+
+//        String indexes = scanner.nextLine();
+//        int swapIndex1 = Integer.parseInt(indexes.split("\\s+")[0]);
+//        int swapIndex2 = Integer.parseInt(indexes.split("\\s+")[1]);
+//        box.swap(swapIndex1, swapIndex2);
+
+        Double element = Double.parseDouble(scanner.nextLine());
+        System.out.println(box.countGreaterThan(element));
+    }
+}
