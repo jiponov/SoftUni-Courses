367f778ca759a32e424e517baac139cd7fe231ca
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-09-04 23:29:15 +0300

    java

---
+++ 09-Java-Advanced/P08_IteratorsAndComparators/EXE_04_Froggy/Main.java
@@ -0,0 +1,29 @@
+package P08_IteratorsAndComparators.EXE_04_Froggy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //1, 2, 3, 4, 5, 6, 7, 8
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Lake lake = new Lake(numbers);
+
+        List<String> output = new ArrayList<>();
+        for (Integer number : lake) {
+            output.add(number.toString());
+        }
+
+        System.out.println(String.join(", ", output));
+    }
+}
