d8b98939d712555e36b7d22711f0ee23c4d00912
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-09-06 06:37:13 +0300

    java

---
+++ 09-Java-Advanced/P08_IteratorsAndComparators/EXE_06_StrategyPattern/Main.java
@@ -0,0 +1,31 @@
+package P08_IteratorsAndComparators.EXE_06_StrategyPattern;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        TreeSet<Person> byAgeOrdered = new TreeSet<>(new PersonAgeComparator());
+        TreeSet<Person> byNameOrdered = new TreeSet<>(new PersonNameComparator());
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int row = 0; row < n; row++) {
+
+            // {name} {age}
+            String input = scanner.nextLine();
+
+            String name = input.split(" ")[0];
+            int age = Integer.parseInt(input.split(" ")[1]);
+            Person person = new Person(name, age);
+            byAgeOrdered.add(person);
+            byNameOrdered.add(person);
+        }
+
+        byNameOrdered.forEach(System.out::println);
+        byAgeOrdered.forEach(System.out::println);
+    }
+}
