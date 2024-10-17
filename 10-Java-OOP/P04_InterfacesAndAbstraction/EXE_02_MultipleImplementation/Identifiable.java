b05164e43bcf1db092af9567a62a25eb42e2c32f
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-10-17 13:45:49 +0300

    java

---
+++ 10-Java-OOP/P04_InterfacesAndAbstraction/EXE_02_MultipleImplementation/Identifiable.java
@@ -0,0 +1,8 @@
+package P04_InterfacesAndAbstraction.EXE_02_MultipleImplementation;
+// package EXE_02_MultipleImplementation;
+
+
+public interface Identifiable {
+
+    String getId();
+}
---
+++ 10-Java-OOP/P04_InterfacesAndAbstraction/EXE_02_MultipleImplementation/Person.java
@@ -0,0 +1,9 @@
+package P04_InterfacesAndAbstraction.EXE_02_MultipleImplementation;
+// package EXE_02_MultipleImplementation;
+
+public interface Person {
+
+    String getName();
+
+    int getAge();
+}
