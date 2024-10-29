4843b1866e8ab20fe2a436ff3a6248cee74bdb6d
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-10-29 14:15:55 +0200

    java

---
+++ 10-Java-OOP/P05_Polymorphism/LAB_01_MathOperation/MathOperation.java
@@ -0,0 +1,43 @@
+package P05_Polymorphism.LAB_01_MathOperation;
+
+public class MathOperation {
+
+
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+
+
+    public int add(int a, int b, int c) {
+
+        return add(a, b) + c;
+    }
+
+
+    public int add(int a, int b, int c, int d) {
+
+        return add(a, b, c) + d;
+    }
+
+
+    public int add(double a, int b) {
+
+        return 0;
+    }
+
+
+    public int add(int a, double b) {
+
+        return 0;
+    }
+
+    //    public String getDocument(String title, String header, String body, String footer) {
+    //        return title + he....;
+    //    }
+    //
+    //    public String getDocument(String title, String body) {
+    //        return getDocument(title, null, body, null);
+    //    }
+
+}
---
+++ 10-Java-OOP/P05_Polymorphism/LAB_01_MathOperation/MathOperationExtended.java
@@ -0,0 +1,11 @@
+package P05_Polymorphism.LAB_01_MathOperation;
+
+public class MathOperationExtended extends MathOperation {
+
+
+    public long add(int a, long b) {
+
+        return a + b;
+    }
+
+}
