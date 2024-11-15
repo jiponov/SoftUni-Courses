3d7a1d5a2cbed319e1ddacc036e3d40eb24cf596
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-11-15 11:04:17 +0200

    mysql

---
+++ 11-MySQL/01_Databases/EXE_04TruncateTableMinions.sql
@@ -0,0 +1,9 @@
+USE `minions`;
+/*  send to JUDGE only statements below, without USE clause  */
+
+TRUNCATE TABLE `minions`;
+
+
+/* второ решение 
+TRUNCATE TABLE minions.minions;
+*/
---
+++ 11-MySQL/01_Databases/EXE_05DropAllTables.sql
@@ -0,0 +1,11 @@
+USE `minions`;
+/*  send to JUDGE only statements below, without USE clause  */
+
+DROP TABLE `minions`;
+DROP TABLE `towns`;
+
+
+/* второ решение 
+DROP TABLE minions.minions;
+DROP TABLE minions.towns;
+*/
