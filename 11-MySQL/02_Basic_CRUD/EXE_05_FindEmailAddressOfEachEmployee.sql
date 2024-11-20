b5fe71aa9809bd136a0a296856bee5128ef63b64
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-11-20 22:57:08 +0200

    java

---
+++ 11-MySQL/02_Basic_CRUD/EXE_05_FindEmailAddressOfEachEmployee.sql
@@ -0,0 +1,11 @@
+SELECT CONCAT(first_name, '.', last_name, '@softuni.bg') AS full_email_address
+FROM soft_uni2.employees
+
+
+/*
+
+SELECT CONCAT(first_name, '.', last_name, '@softuni.bg') AS full_email_address
+FROM employees
+
+
+*/
---
+++ 11-MySQL/02_Basic_CRUD/EXE_06_FindAllDifferentEmployeesSalaries.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT salary
+FROM soft_uni2.employees;
+
+/*
+SELECT DISTINCT salary
+FROM employees;
+*/
