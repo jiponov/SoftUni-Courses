5bd38d4cdb4a36ea846ac323cccf0284e0cb6750
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-11-22 23:35:01 +0200

    mysql

---
+++ 11-MySQL/02_Basic_CRUD/EXE_07_FindAllInformationAboutEmployees.sql
@@ -0,0 +1,9 @@
+SELECT * FROM soft_uni2.employees
+WHERE job_title = 'Sales Representative';
+
+/*
+
+SELECT * FROM employees
+WHERE job_title = 'Sales Representative';
+
+*/
---
+++ 11-MySQL/02_Basic_CRUD/EXE_08_FindNamesOfAllEmployeesBySalaryInRange.sql
@@ -0,0 +1,9 @@
+SELECT first_name, last_name, job_title FROM soft_uni2.employees
+WHERE salary >= 20000 AND salary <= 30000;
+
+/*
+
+SELECT first_name, last_name, job_title FROM employees
+WHERE salary >= 20000 AND salary <= 30000;
+
+*/
