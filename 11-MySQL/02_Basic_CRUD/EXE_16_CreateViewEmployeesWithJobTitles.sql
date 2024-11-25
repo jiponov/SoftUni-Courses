ef4cbbd916d78633f867e611afdb9fb3c0012976
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-11-26 00:22:29 +0200

    mysql

---
+++ 11-MySQL/02_Basic_CRUD/EXE_16_CreateViewEmployeesWithJobTitles.sql
@@ -0,0 +1,15 @@
+CREATE VIEW soft_uni2.v_employees_job_titles AS
+SELECT
+    CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name,
+    job_title
+FROM soft_uni2.employees;
+
+/*
+
+CREATE VIEW v_employees_job_titles AS
+SELECT
+    CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name,
+    job_title
+FROM employees;
+
+*/
---
+++ 11-MySQL/02_Basic_CRUD/EXE_17_DistinctJobTitles.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT job_title
+FROM soft_uni2.employees
+ORDER BY job_title;
+
+/*
+
+SELECT DISTINCT job_title
+FROM employees
+ORDER BY job_title;
+
+*/
