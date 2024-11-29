9bff8e7e04f5de9d3f7832a216bd9b3400753507
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-11-29 20:12:20 +0200

    mysql

---
+++ 11-MySQL/02_Basic_CRUD/EXE_19_Last7HiredEmployees.sql
@@ -0,0 +1,11 @@
+SELECT first_name, last_name, hire_date
+FROM soft_uni2.employees
+ORDER BY hire_date DESC LIMIT 7;
+
+/*
+
+SELECT first_name, last_name, hire_date
+FROM employees
+ORDER BY hire_date DESC LIMIT 7;
+
+*/
---
+++ 11-MySQL/02_Basic_CRUD/EXE_20_IncreaseSalaries.sql
@@ -0,0 +1,13 @@
+UPDATE soft_uni2.employees
+SET salary = salary * 1.12
+WHERE department_id IN(1,2,4,11);
+
+SELECT salary FROM soft_uni2.employees;
+
+/*
+UPDATE employees
+SET salary = salary * 1.12
+WHERE department_id IN(1,2,4,11);
+
+SELECT salary FROM employees;
+*/
