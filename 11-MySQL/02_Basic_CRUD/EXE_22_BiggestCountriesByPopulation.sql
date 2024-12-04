20d282d7a893b07e157a7e41652cb1df58b2aae4
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-12-04 14:33:36 +0200

    mysql

---
+++ 11-MySQL/02_Basic_CRUD/EXE_22_BiggestCountriesByPopulation.sql
@@ -0,0 +1,13 @@
+SELECT country_name, population
+FROM geography.countries
+WHERE continent_code = 'EU'
+ORDER BY population DESC, country_name LIMIT 30;
+
+/*
+
+SELECT country_name, population
+FROM countries
+WHERE continent_code = 'EU'
+ORDER BY population DESC, country_name LIMIT 30;
+
+*/
