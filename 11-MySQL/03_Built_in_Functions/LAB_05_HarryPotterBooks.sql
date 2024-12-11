8796d5cf5cb49470130a0ce7d0e79b858b140a07
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2024-12-11 23:57:28 +0200

    mysql

---
+++ 11-MySQL/03_Built_in_Functions/LAB_05_HarryPotterBooks.sql
@@ -0,0 +1,17 @@
+USE book_library;
+
+SELECT title
+FROM books
+WHERE title LIKE 'harry potter%';
+
+/*
+SELECT title
+FROM book_library.books
+WHERE title LIKE 'Harry Potter%';
+*/
+
+/* --for JUDGE
+SELECT title
+FROM books
+WHERE title LIKE 'harry potter%';
+*/
