4945a953c6690b834869e61eab8161b4a1a8dd51
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-01-16 15:13:17 +0200

    mysql

---
+++ 11-MySQL/04_Data_Aggregation/EXERCISE-06-DepositsSumForOllivanderFamily.sql
@@ -0,0 +1,20 @@
+SELECT
+    deposit_group,
+    SUM(deposit_amount) AS `total_sum`
+FROM gringotts.wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group;
+
+
+/*
+
+SELECT
+    deposit_group,
+    SUM(deposit_amount) AS `total_sum`
+FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group;
+
+*/
