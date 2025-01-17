fd4e37b571562edb713908c15e020c14b000c656
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-01-18 00:29:31 +0200

    mysql

---
+++ 11-MySQL/04_Data_Aggregation/EXERCISE-10-FirstLetter.sql
@@ -0,0 +1,17 @@
+SELECT
+    LEFT(first_name, 1) AS 'first_letter'
+FROM gringotts.wizzard_deposits
+WHERE deposit_group = 'Troll Chest'
+GROUP BY first_letter
+ORDER BY first_letter;
+
+/*
+
+SELECT
+    LEFT(first_name, 1) AS 'first_letter'
+FROM wizzard_deposits
+WHERE deposit_group = 'Troll Chest'
+GROUP BY first_letter
+ORDER BY first_letter;
+
+*/
