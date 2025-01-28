73a7d140ad7bc713a2515bc5d319fabb04aa7b34
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-01-28 12:38:25 +0200

    mysql

---
+++ 11-MySQL/04_Data_Aggregation/EXERCISE-14-EmployeesMaximumSalaries.sql
@@ -0,0 +1,20 @@
+SELECT
+    department_id,
+    MAX(salary) AS 'max_salary'
+FROM soft_uni4.employees
+GROUP BY department_id
+HAVING max_salary < 30000 OR max_salary > 70000
+ORDER BY department_id;
+
+
+/*
+
+SELECT
+    department_id,
+    MAX(salary) AS 'max_salary'
+FROM employees
+GROUP BY department_id
+HAVING max_salary < 30000 OR max_salary > 70000
+ORDER BY department_id;
+
+*/
