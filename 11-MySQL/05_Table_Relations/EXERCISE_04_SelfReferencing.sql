3cbebdb37c57fb7e559e6726ddf3ddb7f57f6f45
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-02-02 15:03:49 +0200

    mysql

---
+++ 11-MySQL/05_Table_Relations/EXERCISE_04_SelfReferencing.sql
@@ -0,0 +1,39 @@
+CREATE TABLE geography3.teachers (
+    teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255),
+    manager_id INT
+);
+
+INSERT INTO geography3.teachers(teacher_id, name, manager_id) VALUES
+(101, 'John', null),
+(102, 'Maya', 106),
+(103, 'Silvia', 106),
+(104, 'Ted', 105),
+(105, 'Mark', 101),
+(106, 'Greta', 101);
+
+ALTER TABLE geography3.teachers
+    ADD FOREIGN KEY (manager_id)
+        REFERENCES teachers (teacher_id);
+        
+/*
+        
+        CREATE TABLE teachers (
+    teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255),
+    manager_id INT
+);
+
+INSERT INTO teachers(teacher_id, name, manager_id) VALUES
+(101, 'John', null),
+(102, 'Maya', 106),
+(103, 'Silvia', 106),
+(104, 'Ted', 105),
+(105, 'Mark', 101),
+(106, 'Greta', 101);
+
+ALTER TABLE teachers
+    ADD FOREIGN KEY (manager_id)
+        REFERENCES teachers (teacher_id);
+        
+        */
