b180ca3dad68bece6f3a0510a79fb3235d5aef91
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-02-08 13:20:10 +0200

    mysql

---
+++ 11-MySQL/06_Subqueries_and_JOINs/EXERCISE_06_EmployeesHiredAfter.sql
@@ -0,0 +1,23 @@
+SELECT
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    d.name AS `dept_name`
+FROM soft_uni6.employees AS e
+      JOIN soft_uni6.departments AS d ON d.department_id = e.department_id
+WHERE e.hire_date > '1999-01-01' AND d.name IN ('Sales', 'Finance')
+ORDER BY e.hire_date;
+
+/*
+
+SELECT
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    d.name AS `dept_name`
+FROM employees AS e
+      JOIN departments AS d ON d.department_id = e.department_id
+WHERE hire_date > '1999-01-01' AND name IN ('Sales', 'Finance')
+ORDER BY hire_date;
+
+*/
