a29ed4634058e48487ce23bdccfb883aed8f689c
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-02-11 22:10:38 +0200

    mysql

---
+++ 11-MySQL/06_Subqueries_and_JOINs/EXERCISE_09_EmployeeManager.sql
@@ -0,0 +1,23 @@
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.manager_id,
+    m.first_name
+FROM soft_uni6.employees AS e
+     JOIN soft_uni6.employees AS m ON e.manager_id = m.employee_id
+WHERE e.manager_id IN (3, 7)
+ORDER BY e.first_name;
+
+/*
+
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.manager_id,
+    m.first_name
+FROM employees AS e
+     JOIN employees AS m ON e.manager_id = m.employee_id
+WHERE e.manager_id IN (3, 7)
+ORDER BY e.first_name;
+
+*/
