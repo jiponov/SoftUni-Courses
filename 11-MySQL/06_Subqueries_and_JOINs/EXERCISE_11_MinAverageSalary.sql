a5bdbc25c686729b2c66546887cf2d7884d00b3b
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-02-12 23:05:53 +0200

    mysql

---
+++ 11-MySQL/06_Subqueries_and_JOINs/EXERCISE_11_MinAverageSalary.sql
@@ -0,0 +1,14 @@
+SELECT MIN(avg_salary) AS lowest_avg_salary
+FROM (SELECT AVG(salary) AS avg_salary
+      FROM soft_uni6.employees
+      GROUP BY department_id) AS department_averages;
+      
+      
+      /*
+      
+      SELECT MIN(avg_salary) AS lowest_avg_salary
+FROM (SELECT AVG(salary) AS avg_salary
+      FROM employees
+      GROUP BY department_id) AS department_averages;
+      
+      */
