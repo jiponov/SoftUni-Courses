e1d6fdd43697be9d5306ebbd7ea92f08490b27a1
Lubomir Jiponov <lubomir.jiponov@gmail.com>
2025-02-23 07:34:31 +0200

    mysql

---
+++ 11-MySQL/07_Database_Programmability_and_Transactions/EXERCISE_04_EmployeesFromTown.sql
@@ -0,0 +1,30 @@
+DELIMITER $$
+CREATE PROCEDURE soft_uni8.usp_get_employees_from_town(`town_name` VARCHAR(255))
+BEGIN
+    SELECT
+        e.first_name,
+        e.last_name
+    FROM soft_uni8.employees AS e
+             JOIN soft_uni8.addresses AS a ON a.address_id = e.address_id
+             JOIN soft_uni8.towns AS t ON t.town_id = a.town_id
+    WHERE t.name = town_name
+    ORDER BY e.first_name, e.last_name, e.employee_id;
+END$$
+DELIMITER ;
+
+/*
+
+
+CREATE PROCEDURE usp_get_employees_from_town(`town_name` VARCHAR(255))
+BEGIN
+    SELECT
+        e.first_name,
+        e.last_name
+    FROM employees AS e
+             JOIN addresses AS a ON a.address_id = e.address_id
+             JOIN towns AS t ON t.town_id = a.town_id
+    WHERE t.name = town_name
+    ORDER BY e.first_name, e.last_name, e.employee_id;
+END
+
+*/
